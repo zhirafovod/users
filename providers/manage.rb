91984b918ca1f8661727778bbc1a4cb32ce521d7
Sergey Sergeev <ssergeev@splunk.com>
2014-05-18 00:47:46 -0700

    fix logic

--- providers/manage.rb
+++ providers/manage.rb
@@ -99,10 +99,13 @@ action :create do
         shell u['shell']
         comment u['comment']
         password u['password'] if u['password']
-        supports :manage_home => true if (home_dir != "/dev/null") and (new_resource.manage_home)
+        if (home_dir == "/dev/null") 
+          supports :manage_home => false
+        else
+          supports :manage_home => new_resource.manage_home
+        end
         home home_dir
         action u['action'] if u['action']
-        manage
       end
 
       if manage_home_files?(home_dir, u['username'])
